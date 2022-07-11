7dce0f8937135d9471b9a41e38beb54724ec8c23
TatsianaN <tatyana.nelipovich@gmail.com>
2022-07-11 21:23:01 +0200

    feat: sidenav component created, material.shared module created

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatRadioModule } from '@angular/material/radio';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+    exports: [MatSidenavModule, MatRadioModule],
+})
+export class MaterialModule {}
