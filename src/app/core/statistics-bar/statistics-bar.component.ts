ebff91be9ebb811ea1c626acf7a60358165f7328
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-13 12:52:41 +0200

    fix: fixed PR remarks

--- src/app/core/statistics-bar/statistics-bar.component.ts
+++ src/app/core/statistics-bar/statistics-bar.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-statistics-bar',
   templateUrl: './statistics-bar.component.html',
   styleUrls: ['./statistics-bar.component.scss']
 })
-export class StatisticsBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class StatisticsBarComponent {
+  statisticsBarItems = [{
+    name: 'Notifications',
+    url: '',
+    icon: 'notifications',
+  },
+  {
+    name: 'Disable alerts',
+    url: '',
+    icon: 'notifications_off',
+  },
+  {
+    name: 'Links',
+    url: '',
+    icon: 'link',
+  },
+  {
+    name: 'Statistics',
+    url: '',
+    icon: 'bar_chart',
+  },
+] 
 }
