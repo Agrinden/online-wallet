4cd81c7ef1a09d1501d28ab489a1ae9420f7e309
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-14 21:05:30 +0200

    Card#12: app-routing module lazy-loaded added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouteUrls } from './core/constants/routes';
+import { AuthGuard } from './core/guards/auth.guard';
+import { NotFoundComponent } from './modules/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: RouteUrls.budget,
+        loadChildren: () => import('./modules/budget/budget.module').then((m) => m.BudgetModule),
+        canActivate: [AuthGuard],
+    },
+
+    {
+        path: RouteUrls.expenses,
+        loadChildren: () => import('./modules/expense/expense.module').then((m) => m.ExpenseModule),
+        canActivate: [AuthGuard],
+    },
+
+    {
+        path: RouteUrls.savings,
+        loadChildren: () => import('./modules/savings/savings.module').then((m) => m.SavingsModule),
+        canActivate: [AuthGuard],
+    },
+
+    {
+        path: RouteUrls.statistics,
+        loadChildren: () => import('./modules/statistics/statistics.module').then((m) => m.StatisticsModule),
+        canActivate: [AuthGuard],
+    },
+
+    {
+        path: '**',
+        component: NotFoundComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
