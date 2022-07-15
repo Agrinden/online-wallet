9ed3d6fc6915ec7ca907fd4c3aa32cc665d5ed9f
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-15 17:42:02 +0200

    Card#12: layout login and home added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
-import { MaterialModule } from '@app/shared/material.module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from '@app/layouts/home-layout/home-layout.component';
+import { LoginLayoutComponent } from '@app/layouts/login-layout/login-layout.component';
+import { MaterialModule } from '@app/shared/material.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    MaterialModule
-  ],
-  exports: [MaterialModule]
+    declarations: [],
+    imports: [CommonModule, MaterialModule, RouterModule],
+    exports: [MaterialModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
