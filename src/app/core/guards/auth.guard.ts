371186a1cf17ec650c42a93d4f693798657f62bd
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-18 11:43:40 +0200

    Card#12: fix pr remarks

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -7,10 +7,9 @@ import {
     Router,
     RouterStateSnapshot,
     UrlSegment,
-    UrlTree,
 } from '@angular/router';
 import { RouteUrls } from '@app/core/constants/routes';
-import { map, Observable, first, of } from 'rxjs';
+import { first, map, Observable, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -19,20 +18,14 @@ export class AuthGuard implements CanActivate, CanLoad {
     private isAuth$: Observable<boolean> = of(true);
 
     constructor(private router: Router) {}
-    canLoad(
-        route: Route,
-        segments: UrlSegment[]
-    ): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.getIsAuth();
+    canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+        return this.getIsAuth$();
     }
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return this.getIsAuth();
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this.getIsAuth$();
     }
 
-    private getIsAuth(): Observable<boolean> {
+    private getIsAuth$(): Observable<boolean> {
         return this.isAuth$.pipe(
             first(),
             map((isAuth) => {
