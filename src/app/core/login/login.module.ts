d7fc198483fc6c89c2e02b528ab494efba6d1690
Grinden <bce.gomel@gmail.com>
2022-07-19 13:31:20 +0200

    [#5/US.MAN-01] Created eye hidden button, added validators, added forgot link

--- src/app/core/login/login.module.ts
+++ src/app/core/login/login.module.ts
@@ -1,25 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 import { LoginComponent } from './components/login.component';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: LoginComponent
-      }
-  ])],
+    declarations: [LoginComponent],
+    imports: [
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        CommonModule,
+        MatIconModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: LoginComponent,
+            },
+        ]),
+    ],
 })
 export class LoginModule {}
