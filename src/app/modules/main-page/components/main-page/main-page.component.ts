71c7cdedd10c47c982bb5596631e5ff2952ca121
Grinden <bce.gomel@gmail.com>
2022-07-25 21:59:16 +0200

    [INC-01] Resolved conflicts, deleted png, nesting in SCSS

--- src/app/modules/main-page/components/main-page/main-page.component.ts
+++ src/app/modules/main-page/components/main-page/main-page.component.ts
@@ -1,3 +1,6 @@
+import { filter } from 'rxjs/operators';
+import { IncomeComponent } from './../../../../shared/income/components/income.component';
+import { MatDialog } from '@angular/material/dialog';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +8,14 @@ import { Component } from '@angular/core';
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+    constructor(private dialog: MatDialog) {}
+
+    public openIncomeForm(): void {
+        this.dialog
+            .open(IncomeComponent)
+            .beforeClosed()
+            .pipe(filter((data) => !!data))
+            .subscribe();
+    }
+}
