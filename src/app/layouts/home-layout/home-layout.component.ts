6b3aa0c445d64edf2aba03a07450601912194223
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-25 15:19:57 +0200

    fix: fixed pr: moved tabs template from home-layout to header

--- src/app/layouts/home-layout/home-layout.component.ts
+++ src/app/layouts/home-layout/home-layout.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { menuTabs } from '@core/constants';
 
 @Component({
     selector: 'app-home-layout',
     templateUrl: './home-layout.component.html',
     styleUrls: ['./home-layout.component.scss'],
 })
-export class HomeLayoutComponent {
-    public menuItems = menuTabs;
-    public activeLink = menuTabs[0];
-}
+export class HomeLayoutComponent {}
