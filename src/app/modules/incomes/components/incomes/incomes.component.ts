52f7eb15f1d7928b031e84e33245e70ca075f6f8
Grinden <bce.gomel@gmail.com>
2022-07-25 23:36:42 +0200

    [INC-01] Added IncomeComponent to Incomes page

--- src/app/modules/incomes/components/incomes/incomes.component.ts
+++ src/app/modules/incomes/components/incomes/incomes.component.ts
@@ -1,3 +1,6 @@
+import { filter } from 'rxjs/operators';
+import { IncomeComponent } from './../../../../shared/income/components/income.component';
+import { MatDialog } from '@angular/material/dialog';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +8,14 @@ import { Component } from '@angular/core';
     templateUrl: './incomes.component.html',
     styleUrls: ['./incomes.component.scss'],
 })
-export class IncomesComponent {}
+export class IncomesComponent {
+    constructor(private dialog: MatDialog) {}
+
+    public openIncomeForm(): void {
+        this.dialog
+            .open(IncomeComponent)
+            .beforeClosed()
+            .pipe(filter((data) => !!data))
+            .subscribe();
+    }
+}
