b3e052e49bdbdcd62281d96a751d6f738fb3965f
RuslanTanasiichuk <74545458+RuslanTanasiichuk@users.noreply.github.com>
2022-07-27 18:48:58 +0300

    [Card#11 https://trello.com/c/Z7jKsGaW] Google authentication (#17)
    * feat: implemented log in via google. secured client id in containerized environment
    
    * feat: added log out functionality and verification of authorization by Google
    
    * fix: call to start authentication flow moved to separate method
    
    * fix: changed subscription to empty
    
    * fix: fixed imports in app.module

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 import { RouteUrls } from '@core/constants';
 import { CoreModule } from '@core/core.module';
 import { AccessTokenService } from '@core/services';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, of } from 'rxjs';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Injectable({
     providedIn: CoreModule,
@@ -12,7 +13,11 @@ export class UserService {
     private readonly userSubject$ = new BehaviorSubject<any | null>(null);
     readonly activeUser$ = this.userSubject$.asObservable();
 
-    constructor(private accessTokenService: AccessTokenService, private router: Router) {}
+    constructor(
+        private accessTokenService: AccessTokenService,
+        private router: Router,
+        private oidcSecurityService: OidcSecurityService
+    ) {}
 
     get user(): any | null {
         return this.userSubject$.getValue();
@@ -25,11 +30,20 @@ export class UserService {
     signOut(): void {
         this.user = null;
         this.accessTokenService.clear();
+        this.oidcSecurityService.logoff();
         this.router.navigate([RouteUrls.login]);
     }
 
     public get isLoggedIn$(): Observable<boolean> {
         // return this.activeUser$.pipe(map((user) => !!user));
-        return of(true);
+
+        return combineLatest([
+            this.oidcSecurityService.checkAuth().pipe(map(({ isAuthenticated }) => isAuthenticated)),
+            of(true),
+        ]).pipe(
+            map(([isLoggedInByGoogle, isLoggedIn]) => {
+                return isLoggedInByGoogle || isLoggedIn;
+            })
+        );
     }
 }
