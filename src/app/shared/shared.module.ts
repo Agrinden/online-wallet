97379b47165cf04033e791355db3b880b80e2ff2
Grinden <bce.gomel@gmail.com>
2022-07-28 17:50:46 +0200

    [INC-01-1] added new category button with with modal window

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { AddCategoryComponent } from './add-category/components/add-category.component';
 import { OpenIncomeFormComponent } from './open-income-form/components/open-income-form.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -6,10 +7,18 @@ import { MaterialModule } from '@app/shared/material.module';
 import { DialogModule } from './dialog/dialog.module';
 
 import { DialogComponent } from '@shared/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [DialogComponent, OpenIncomeFormComponent],
-    imports: [CommonModule, MaterialModule, RouterModule, DialogModule],
-    exports: [MaterialModule, RouterModule, DialogComponent, DialogComponent, OpenIncomeFormComponent],
+    declarations: [DialogComponent, OpenIncomeFormComponent, AddCategoryComponent],
+    imports: [CommonModule, MaterialModule, RouterModule, DialogModule, ReactiveFormsModule],
+    exports: [
+        MaterialModule,
+        RouterModule,
+        DialogComponent,
+        DialogComponent,
+        OpenIncomeFormComponent,
+        AddCategoryComponent,
+    ],
 })
 export class SharedModule {}
