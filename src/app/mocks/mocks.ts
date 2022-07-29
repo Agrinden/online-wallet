b097eca1f6c07a2bb3db7371fffea4f043acf148
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-29 13:11:03 +0200

    refactor: deleted redundant file of categories

--- src/app/mocks/mocks.ts
+++ src/app/mocks/mocks.ts
@@ -50,3 +50,47 @@ export const CATEGORIES: Observable<CategoryInterface[]> = of([
     },
 ]);
 export const CURRENCIES: CurrencyInterface[] = [{ name: 'EUR' }, { name: 'GEL' }, { name: 'PLN' }, { name: 'USD' }];
+
+export const categories = [
+    {
+        name: 'Home & utilities',
+        subcategories: ['Mortgage', 'Rent', 'Utilities', 'Internet', 'Mobile'],
+    },
+    {
+        name: 'Insurance & financial',
+        subcategories: ['Car insurance', 'Health insurance', 'Loan', 'Bank deposit'],
+    },
+    {
+        name: 'Food',
+        subcategories: ['Groseries', 'Eating out'],
+    },
+    {
+        name: 'Entertainment',
+        subcategories: [
+            'Coffee & tea',
+            'Coffee & tea',
+            'Restaurants',
+            'Books',
+            'Movies & music',
+            'Subscriptions',
+            'Celebrations & gifts',
+        ],
+    },
+    {
+        name: 'Transport & auto',
+        subcategories: ['Public transport & train', 'Petrol', 'Road tolls & parking'],
+    },
+
+    {
+        name: 'Children',
+        subcategories: ['Baby products', 'Toys for kids', 'School'],
+    },
+    {
+        name: 'Education',
+        subcategories: [],
+    },
+    {
+        name: 'Pets',
+        subcategories: [],
+    },
+];
