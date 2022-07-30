3c86b29c45aa7f284085ed89238e2df3c9b92f18
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-07-30 21:30:19 +0200

    fix: fixed service providedin

--- src/app/core/services/warn-dialog/warning-dialog.service.ts
+++ src/app/core/services/warn-dialog/warning-dialog.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { CoreModule } from '@app/core/core.module';
 import { DialogComponent } from '@app/shared';
 import { ConfirmationDialogChoise } from '@app/shared/enums/dialog-enums';
 import { filter, take } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root',
+    providedIn: CoreModule,
 })
 export class WarningDialogService {
     constructor(private dialog: MatDialog) {}
