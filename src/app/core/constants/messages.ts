4145a99dabe740f809bfeb5ceea6381b4118cd15
RuslanTanasiichuk <ivcha.ruslan@gmail.com>
2022-08-10 13:54:50 +0300

    feat: deleting wallet

--- src/app/core/constants/messages.ts
+++ src/app/core/constants/messages.ts
@@ -6,4 +6,6 @@ export const deleteCategoryMessage = 'You are about to delete category. Are you 
 export const deleteCategorySuccessMessage = 'You category successfully deleted';
 export const EditCategorySuccessMessage = 'You category successfully updated';
 export const shouldCreateWallet = 'Please, create a wallet and after that you can return to adding your transaction';
-
+export const deleteWalletMessage = 'Are you sure you want to delete this wallet?';
+export const deleteDefaultWalletMessage =
+    'This wallet is default, first you need to change the status to non-default and then proceed with deletion';
