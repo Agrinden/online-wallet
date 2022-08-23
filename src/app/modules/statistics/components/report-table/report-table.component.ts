f9c1c43d68d7f9ab7824c89b80ff3bb189aa9538
RuslanTanasiichuk <74545458+RuslanTanasiichuk@users.noreply.github.com>
2022-08-23 11:43:04 +0300

    [Feature REP-02] Custom Report Table (#46)
    * feat: implemented report table
    
    * fix: changed column interface name

---
+++ src/app/modules/statistics/components/report-table/report-table.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { ReportInterface } from '@shared/interfaces/custom-report-interface';
+
+interface ReportTableItemInterface {
+    category?: string;
+    income?: number;
+    expense?: number;
+    payer?: string;
+    subTableTitle?: string;
+}
+interface ColumnInterface {
+    dataField: keyof ReportTableItemInterface;
+    title: string;
+}
+
+@Component({
+    selector: 'app-report-table',
+    templateUrl: './report-table.component.html',
+    styleUrls: ['./report-table.component.scss'],
+})
+export class ReportTaleComponent implements OnChanges {
+    @Input() report?: ReportInterface;
+    public readonly columns: ColumnInterface[] = [
+        {
+            title: '',
+            dataField: 'category',
+        },
+        {
+            title: 'Income',
+            dataField: 'income',
+        },
+        {
+            title: 'Expense',
+            dataField: 'expense',
+        },
+        {
+            title: 'Payer',
+            dataField: 'payer',
+        },
+    ];
+    public readonly displayedColumns = this.columns.map(({ dataField }) => dataField);
+    public reportTableData?: ReportTableItemInterface[];
+
+    public ngOnChanges() {
+        if (!this.report) {
+            return;
+        }
+
+        this.reportTableData = [
+            { subTableTitle: 'Expense categories' },
+            ...this.report.expense.map(({ category, amount, payer }) => ({ category, payer, expense: amount })),
+            { subTableTitle: 'Incomes categories' },
+            ...this.report.income.map(({ category, amount }) => ({ category, income: amount })),
+        ];
+    }
+
+    public isSubTableRow(index: number, row: any): boolean {
+        return !!row.subTableTitle;
+    }
+}
--- src/app/modules/statistics/components/statistics/statistics.component.ts
+++ src/app/modules/statistics/components/statistics/statistics.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { ReportInterface } from '@shared/interfaces/custom-report-interface';
+import { mockReport } from '@app/mocks';
 
 @Component({
     selector: 'app-statistics',
     templateUrl: './statistics.component.html',
     styleUrls: ['./statistics.component.scss'],
 })
-export class StatisticsComponent {}
+export class StatisticsComponent {
+    public readonly report: ReportInterface = mockReport;
+}
