5f369aa97475796712b5d60c70ddfa7a16bf2976
TatsianaN <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtQ+0Fow/jnyGugjRjkUlFqEnhs4k5SGIEZYsRh/mBXr7fAqUemme9y01S27iBzBw0716O+9msudD92CSgiP3DqZ/k60XBF23NCLlauoM6bzl21Fyyx3r6H6wltz3bdgwr5YbwKw2QmBCVUboLMad6Lc4fmiCD380Y4SWADTA2GBWawzs88XZ1m0UsNzxTMw7NQ404dAt44yZsEAkHYsC0yrAZYTSs/qOczZhkVwLzycTF5BCK5K4i880/OrPIFlYpqbVvfWZc52I7HmPbrEDHx+E>
2022-08-25 17:15:44 +0200

    fix: fixed imports

--- src/app/modules/expenses/expenses.module.ts
+++ src/app/modules/expenses/expenses.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { IncomeTableComponent } from '@app/shared';
-import { ViewIncomeListComponent } from '@app/shared/view-income-list/components/view-income-list.component';
 import { CoreModule } from '@core/core.module';
 import { ExpensesComponent, ExpensesRoutingModule } from '@modules/expenses';
 import { SharedModule } from '@shared/shared.module';
 import { MainPageModule } from './../main-page/main-page.module';
 
 @NgModule({
-    declarations: [ExpensesComponent, IncomeTableComponent, ViewIncomeListComponent],
+    declarations: [ExpensesComponent],
     imports: [CommonModule, ExpensesRoutingModule, SharedModule, CoreModule, MainPageModule],
 })
 export class ExpensesModule {}
